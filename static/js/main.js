4a4f3b741e018e7c7eb8177b5e4242247bc127be
BeiYuu <dew.reg@gmail.com>
2013-11-11 11:42:15 +0800

    cancel the auto focus

--- static/js/main.js
+++ static/js/main.js
@@ -48,11 +48,11 @@ $(function(){
         shareInputFocus = false;
     });
 
-    $('body').bind('keydown',function (e) {
-        if (!shareInputFocus && !e.metaKey && !e.shiftKey && !e.ctrlKey && !e.altKey && e.keyCode != 27 && e.keyCode!= 32 && e.keyCode!=33 && e.keyCode!=34 && !$('#searchApi').is(':focus')) {
-            $('#searchApi').focus();
-        }
-    });
+    //$('body').bind('keydown',function (e) {
+        //if (!shareInputFocus && !e.metaKey && !e.shiftKey && !e.ctrlKey && !e.altKey && e.keyCode != 27 && e.keyCode!= 32 && e.keyCode!=33 && e.keyCode!=34 && !$('#searchApi').is(':focus')) {
+            //$('#searchApi').focus();
+        //}
+    //});
 
     var scrollTimeout;
     $('#searchApi').quicksearch('.board-card li', {
